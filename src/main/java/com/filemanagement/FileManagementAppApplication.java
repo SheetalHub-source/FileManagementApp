2e4ae87d73e9672f4d0e11766479112644e87ee0
SheetalHub-source <sheetalrajput3497@gmail.com>
2025-12-25 16:43:54 +0530

    Initial commmit

---
+++ src/main/java/com/filemanagement/FileManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.filemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementAppApplication.class, args);
+	}
+
+}
